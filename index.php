7639bbcd98bc02252842d53893c8f49d0e402d9e
Douglas Bernardo <douglasbernardomelo@gmail.com>
2018-11-07 15:02:40 -0200

    Primeiro commit, alguns ajustes, index criada e css

---
+++ index.php
@@ -0,0 +1,18 @@
+<!Doctype html>
+<html>
+	<head>
+		<title>projeto_01</title>
+		<link rel="stylesheet" type="text/css" href="css/css.css">
+	</head>
+	<body>
+		<div id="header">
+			<h1>1º-Projeto</h1>
+		</div>
+		<div id="content">
+			<a href="#" class="botao1">botao1</a>
+			<a href="#" class="botao2">botao2</a>
+			<a href="#" class="botao3">botao3</a>
+		</div>	
+		<div id="footer"></div>
+	</body>
+</html>
