2848615dabf5ac95a2054975aac8585b67435607
Douglas Bernardo <douglasbernardomelo@gmail.com>
2018-12-05 17:11:12 -0200

    Exemplos de SQL - Insert, Update, Delete e Select

--- Model/exercicio_sql1.php
+++ Model/exercicio_sql1.php
@@ -1,8 +1,13 @@
 <?php
 
-$sql = "INSERT INTO usuarios 
+include "../DB/connect.php";
 
+$sql = "INSERT INTO usuarios (NOME, SOBRENOME, IDADE) VALUES ('Miguelzido', 'da Silva', 30)";
 
+$query = mysqli_query($link, $sql);
 
+if ($query) {
+	echo "Sucesso";
+}
 
 ?>
---
+++ Model/selectUser.php
@@ -0,0 +1,13 @@
+<?php
+
+include '../DB/connect.php';
+
+$sql = "SELECT * FROM usuarios";
+
+$query = mysqli_query($link, $sql);
+
+while($result = mysqli_fetch_array($query)) {
+	echo $result[0]." - ".$result[1]." - ".$result[2]."<br>";
+}
+
+?>
