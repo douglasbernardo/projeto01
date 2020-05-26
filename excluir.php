633f5e347c35f0699b2abea9d664ac1c03bfa7c1
douglas <douglasbernardomelo@gmail.com>
2020-05-26 18:24:58 -0300

    Exclusao usuarios

---
+++ excluir.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once "DB/connect.php";
+try {
+	
+	$id = $_GET['id'];
+$sql = "DELETE FROM usuarios WHERE id = $id";
+$query = mysqli_query($link,$sql);
+
+} catch (Exception $e) {
+	
+}
+
+if($query===true){
+	header('location: listadeusuarios.php');
+}
+
+?>
