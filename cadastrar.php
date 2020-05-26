403e4f6db36ff2fc129407ca5fe6c580905a7baa
douglas <douglasbernardomelo@gmail.com>
2020-05-26 18:23:25 -0300

    cadastro de usuarios

---
+++ cadastrar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cadastrar Usuario</title>
+</head>
+	<body>
+		<form action="inserirUsuario.php" method="post">
+			<label>Nome</label>
+			<input type="text" name="nome">
+			<label>Idade</label>
+			<input type="number" name="idade">
+			<button>Cadastrar</button>
+		</form>
+	</body>
+</html>
