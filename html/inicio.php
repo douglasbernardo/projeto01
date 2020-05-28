28c58704c5e1c51c092342ab9fcfc5866c7f3601
douglas <douglasbernardomelo@gmail.com>
2020-05-28 20:06:00 -0300

    separando html

---
+++ html/inicio.php
@@ -0,0 +1,32 @@
+<!Doctype html>
+<html>
+	<head>
+		<title>PTL projeto_01</title>
+		<link rel="stylesheet" type="text/css" href="css/css.css">
+		<link href="https://fonts.googleapis.com/css?family=Kanit|Roboto" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Rubik+Mono+One" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	</head>
+	<body>
+		<div id="header">
+			<nav class="navbar navbar-expand-lg navbar-dark">
+				<div class="collapse navbar-collapse" id="navbarNavDropdown">
+					<ul class="navbar-nav">
+						<li class="nav-item active">
+							<a class="nav-link" href="index.php">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="listadeusuario.php">Lista De Usuarios</a>
+	                    </li>
+	                    <li class="nav-item">
+							<a class="nav-link" href="cadastrar.php">Cadastrar</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="sobre.php">Sobre</a>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
+		<div id="content">
